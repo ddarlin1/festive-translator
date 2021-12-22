ae1481ed0e70efb2c4a4d5f6c664d922d930180d
darling_d <ddarling212@gmail.com>
2021-12-21 22:33:11 -0500

    Added text animation, new languages, and updated css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const languageSelector = document.getElementById("language-selector")
 const greetingDisplay = document.getElementById("greeting-text")
+
 languageSelector.addEventListener("change", translate)
 
 const greetingsArr = [
@@ -8,8 +9,32 @@ const greetingsArr = [
         greeting: "Merry Christmas!"
     },
     {
+        language: "French",
+        greeting: "Joyeux Noël!"
+    },
+    {
+        language: "German",
+        greeting: "Frohe Weinachten!"
+    },
+    {
+        language: "Italian",
+        greeting: "Buon Natale!"
+    },
+    {
+        language: "Japanese",
+        greeting: "メリークリスマス!"
+    },
+    {
+        language: "Polish",
+        greeting: "Wesołych świąt Bożego Narodzenia!"
+    },
+    {
         language: "Spanish",
-        greeting: "Feliz Navidad!"
+        greeting: "¡Feliz Navidad!"
+    },
+    {
+        language: "Swedish",
+        greeting: "God Jul!"
     },
     {
         language: "Ukrainian",
@@ -23,9 +48,14 @@ const greetingsArr = [
 // Task: 
 // - Write a function to display the correct greeting when a language is selected.
 
-function translate(){
-    
+function translate() {
+    const selectedLanguage = languageSelector.value
+    for (let i = 0; i < greetingsArr.length; i++) {
+        if (greetingsArr[i].language === selectedLanguage) {
+            greetingDisplay.innerText = greetingsArr[i].greeting
+            greetingDisplay.style.animation = 'textAnimate 8s infinite';
+        }
+    }
 }
-
 // Stretch goals:
-// - Animate the message switch.
+// - Animate the message.
